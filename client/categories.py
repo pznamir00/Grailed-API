b1b36d5e29002a3475445fdcb3104d69542f6871
pznamir00 <pznamir00@gmail.com>
2024-09-10 02:04:23 +0200

    chore(): add categories

--- client/categories.py
+++ client/categories.py
@@ -19,3 +19,61 @@ class Tops(Enum):
     SLEEVELESS = "sleeveless"
     SWEATERS_KNITWEAR = "sweaters_knitwear"
     SWEATSHIRTS = "sweatshirts_hoodies"
+
+
+class Bottoms(Enum):
+    CASUAL_PANTS = "casual_pants"
+    CROPPED_PANTS = "cropped_pants"
+    DENIM = "denim"
+    JUMPSUITS = "jumpsuits"
+    LEGGINGS = "leggings"
+    SHORTS = "shorts"
+    SWEATPANTS_JOGGERS = "sweatpants_joggers"
+    SWIMWEAR = "swimwear"
+
+
+class Outerwear(Enum):
+    BOMBERS = "bombers"
+    CLOAKS_CAPES = "cloaks_capes"
+    DENIM_JACKETS = "denim_jackets"
+    HEAVY_COATS = "heavy_coats"
+    LEATHER_JACKETS = "leather_jackets"
+    LIGHT_JACKETS = "light_jackets"
+    PARKAS = "parkas"
+    RAINCOATS = "raincoats"
+    VESTS = "vests"
+
+
+class Footwear(Enum):
+    BOOTS = "boots"
+    FORMAL_SHOES = "formal_shoes"
+    HITOP_SNEAKERS = "hitop_sneakers"
+    LEATHER = "leather"
+    LOWTOP_SNEAKERS = "lowtop_sneakers"
+    SANDALS = "sandals"
+    SLIP_ONS = "slip_ons"
+
+
+class Tailoring(Enum):
+    BLAZERS = "blazers"
+    FORMAL_SHIRTING = "formal_shirting"
+    FORMAL_TROUSERS = "formal_trousers"
+    SUITS = "suits"
+    TUXEDOS = "tuxedos"
+    VESTS = "vests"
+
+
+class Accessories(Enum):
+    BAGS_LUGGAGE = "bags_luggage"
+    BELTS = "belts"
+    GLASSES = "glasses"
+    GLOVES_SCARVES = "gloves_scarves"
+    HATS = "hats"
+    JEWELRY_WATCHES = "jewelry_watches"
+    MISC = "misc"
+    PERIODICALS = "periodicals"
+    SOCKS_UNDERWEAR = "socks_underwear"
+    SUNGLASSES = "sunglasses"
+    SUPREME = "supreme"
+    TIES_POCKETSQUARES = "ties_pocketsquares"
+    WALLETS = "wallets"
